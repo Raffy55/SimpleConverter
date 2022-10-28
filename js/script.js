d88ca3c943212352c2e409d4147f9a875fe65440
RaffyG <guevarrarafael55@gmail.comgit config --global user.email guevarrarafael55@gmail.comgit config --global user.email guevarrarafael55@gmail.com>
2022-10-28 12:21:36 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function kb () {
+    let x = document.getElementById("firstnum").value;
+    const kbresult = x * 1000;
+
+    document.getElementById('answer').innerHTML = kbresult + " kilobytes";
+}
+
+function gb () {
+    let x = document.getElementById("firstnum").value;
+    const gbresult = x / 1000;
+
+    document.getElementById('answer').innerHTML = gbresult + "gigabytes";
+}
+
+function tb () {
+    let x = document.getElementById("firstnum").value;
+    const tbresult = x / 1000000;
+    
+    document.getElementById('answer').innerHTML = tbresult + " terabytes";
+}
